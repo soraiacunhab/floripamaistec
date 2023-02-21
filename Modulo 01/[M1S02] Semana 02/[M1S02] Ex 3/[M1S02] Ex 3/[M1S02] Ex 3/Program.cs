753c159384588bca1b994ae21de127392415b610
soraiacunhab <cunhasoraia2@gmail.com>
2023-02-21 20:41:36 -0300

    atualização

---
+++ Modulo 01/[M1S02] Semana 02/[M1S02] Ex 3/[M1S02] Ex 3/[M1S02] Ex 3/Program.cs
@@ -0,0 +1,18 @@
+﻿using Exercicios03ao06;
+using Exercicios03ao06.Classes.Enum;
+
+ContaBancaria conta = new ContaBancaria();
+
+conta.numero = 2222;
+conta.agencia = 1234;
+conta.nomeDoTitular = "Soraia Cunha";
+conta.tipoConta = TipoContaEnum.POUPANCA;
+
+// testando metodo depositar
+conta.Depositar(200);
+
+// testando metodo sacar 
+conta.Sacar(50);
+
+// testando método exibir saldo
+conta.ExibirSaldo();
