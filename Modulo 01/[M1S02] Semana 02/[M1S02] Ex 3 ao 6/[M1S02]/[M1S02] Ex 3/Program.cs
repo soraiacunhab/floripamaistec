b6d0625f6d225c1b771790e9ee8016423c9e8798
soraiacunhab <cunhasoraia2@gmail.com>
2023-02-21 21:34:28 -0300

    exercícios da semana 02

--- Modulo 01/[M1S02] Semana 02/[M1S02] Ex 3 ao 6/[M1S02]/[M1S02] Ex 3/Program.cs
+++ Modulo 01/[M1S02] Semana 02/[M1S02] Ex 3 ao 6/[M1S02]/[M1S02] Ex 3/Program.cs
@@ -1,18 +1,23 @@
-﻿using Exercicios03ao06;
+﻿// See https://aka.ms/new-console-template for more information
+using _M1S02__Ex_3;
+
+Console.WriteLine("Hello, World!");
+
+using Exercicios03ao06;
 using Exercicios03ao06.Classes.Enum;
 
 ContaBancaria conta = new ContaBancaria();
 
-conta.numero = 2222;
+conta.numero = 1542;
 conta.agencia = 1234;
-conta.nomeDoTitular = "Soraia Cunha";
+conta.nomeDoTitular = "Bruno Costa";
 conta.tipoConta = TipoContaEnum.POUPANCA;
 
 // testando metodo depositar
 conta.Depositar(200);
 
 // testando metodo sacar 
-conta.Sacar(50);
+conta.Saque(50);
 
 // testando método exibir saldo
 conta.ExibirSaldo();
